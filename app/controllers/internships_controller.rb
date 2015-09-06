07c5c05e4eb600144b870fa5a130623c22ee2670
Vinicius <vinicius.venancio@live.com>
2015-09-05 22:55:57 -0300

    vai

--- app/controllers/internships_controller.rb
+++ app/controllers/internships_controller.rb
@@ -2,11 +2,20 @@ class InternshipsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @internships = Internship.all.includes([:enterprise, :courses])
+    @internships = Internship.where("enterprise_id = ?", params[:enterprise_id])
 
-    respond_to do |format|
-      format.json { render json: @internships.order('internships.id desc').to_json(include: [:enterprise, :courses]) }
-    end
+    # respond_to do |format|
+    #   format.json { render json: @internships.order('internships.id desc').to_json(include: [:enterprise, :courses]) }
+    # end
+  end
+
+  def new
+    @internship = Internship.new
+    @internship.enterprise = Enterprise.find(params[:enterprise_id])
+  end
+
+  def create
+
   end
 
 end
--- spec/controllers/internships_controller_spec.rb
+++ spec/controllers/internships_controller_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe InternshipsController, type: :controller do
+  let(:user) { create(:user) }
+  let!(:internship) { create(:internship) }
+
+  before { sign_in user }
+
+  context 'GET #index' do
+    let(:internships) do
+      internships = [internship]
+      internships
+    end
+
+    before { get :index, enterprise_id: internship.enterprise.id }
+
+    it 'assigns all internships of its enteprise' do
+      expect(assigns(:internships)).to match_array internships
+    end
+
+    context 'on view' do
+      render_views
+
+      it 'renders the index template' do
+        expect(response).to render_template(:index)
+      end
+    end
+  end
+
+
+  context 'POST #create' do
+    context 'params' do
+      it do
+        is_expected.to permit(
+          :publication_time, :enterprise_id, :start_date, :end_date,
+          :requirements, :semester, :benefits, :observation, :title,
+          course_ids: []
+          ).for(:create, params: { internship: attributes_for(:internship) })
+      end
+  end
 
 end
